8cdd7737230a8140d69b1625aa9ab4758fa52d64
ICEP-DEV <89523262+ICEP-DEV@users.noreply.github.com>
2025-01-22 21:07:33 +0200

    Update globals.js

--- globals.js
+++ globals.js
@@ -1,7 +1,7 @@
 //DB CRDS
-const USERNAME = "root";
-const PASSWORD = ""
-const DATABASE_HOSTNAME = "localhost";
+const USERNAME = "dbmasteruser";
+const PASSWORD = "ZHD5ToK:|.]OByYU%F5ys3tl-,7#H2XA"
+const DATABASE_HOSTNAME = "ls-23121a88d4cbb089991485b0bec9f393441a0883.cx2su6ewedog.eu-west-2.rds.amazonaws.com";
 //API PORTS
 const HOSTNAME = "mediahostseverip"
 const SOCKETIO_PORT = process.env.PORT || 2000;
@@ -11,8 +11,8 @@ const APP_PORT = process.env.PORT || 4241;
 
 function DATABASE()
 { 
-    if(DATABASE_HOSTNAME == 'localhost')
-        return "localhost"
+    if(DATABASE_HOSTNAME == 'ls-23121a88d4cbb089991485b0bec9f393441a0883.cx2su6ewedog.eu-west-2.rds.amazonaws.com')
+        return "ls-23121a88d4cbb089991485b0bec9f393441a0883.cx2su6ewedog.eu-west-2.rds.amazonaws.com"
     else
         return "hosted"
 
