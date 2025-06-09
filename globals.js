0a0fd73cee4516e7ca229968e858afd8ad17a3d1
Student <icep.memory1@gmail.com>
2025-06-09 10:20:14 +0200

    initial commit

--- globals.js
+++ globals.js
@@ -1,7 +1,7 @@
 //DB CRDS
-const USERNAME = "dbmasteruser";
-const PASSWORD = "ZHD5ToK:|.]OByYU%F5ys3tl-,7#H2XA"
-const DATABASE_HOSTNAME = "ls-23121a88d4cbb089991485b0bec9f393441a0883.cx2su6ewedog.eu-west-2.rds.amazonaws.com";
+const USERNAME = "root";
+const PASSWORD = ""
+const DATABASE_HOSTNAME = "localhost";
 //API PORTS
 const HOSTNAME = "mediahostseverip"
 const SOCKETIO_PORT = process.env.PORT || 2000;
@@ -11,8 +11,8 @@ const APP_PORT = process.env.PORT || 4241;
 
 function DATABASE()
 { 
-    if(DATABASE_HOSTNAME == 'ls-23121a88d4cbb089991485b0bec9f393441a0883.cx2su6ewedog.eu-west-2.rds.amazonaws.com')
-        return "ls-23121a88d4cbb089991485b0bec9f393441a0883.cx2su6ewedog.eu-west-2.rds.amazonaws.com"
+    if(DATABASE_HOSTNAME == 'localhost')
+        return "localhost"
     else
         return "hosted"
 
